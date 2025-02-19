e337ea81e0cb1150e94cc2842264c9ad01ce1378
Takahiro Menju <takam.dev@gmail.com>
2025-02-19 15:21:35 +0900

    arbigent 0.25.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/arbigent.rb
+++ Formula/arbigent.rb
@@ -4,8 +4,8 @@
 class Arbigent < Formula
   desc "Zero to AI agent testing in minutes for Android, iOS, and Web apps. Arbigent's intuitive UI and powerful code interface make it accessible to everyone, while its scenario breakdown feature ensures scalability for even the most complex tasks."
   homepage ""
-  url "https://github.com/takahirom/arbigent/releases/download/0.24.0/arbigent-0.24.0.tar.gz"
-  sha256 "382ed813db88856aa0f07722064c64532ee4a9b24d7d2b2b9fc26e0b7560d15a"
+  url "https://github.com/takahirom/arbigent/releases/download/0.25.0/arbigent-0.25.0.tar.gz"
+  sha256 "02eaaea8bfa171ef8874cc3c83929d46d57dd4dfcbd5008ac1169d13dfcb1d2a"
   license "Apache-2.0"
 
   # depends_on "cmake" => :build
