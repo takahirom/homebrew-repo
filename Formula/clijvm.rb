8f2b9c51e13e3bb838526f717b62df1e74de20dd
Takahiro Menju <takam.dev@gmail.com>
2026-07-02 14:38:38 +0900

    Update clijvm to 0.3.0

--- Formula/clijvm.rb
+++ Formula/clijvm.rb
@@ -1,8 +1,8 @@
 class Clijvm < Formula
   desc "AI-friendly JVM profiler CLI built on JDK Flight Recorder"
   homepage "https://github.com/takahirom/jvm-profiler-cli"
-  url "https://github.com/takahirom/jvm-profiler-cli/releases/download/0.2.0/clijvm-0.2.0.tar.gz"
-  sha256 "89ea1696a8c15a8d1a095614b5902edab469cdac71caba623d1e4047937d43f2"
+  url "https://github.com/takahirom/jvm-profiler-cli/releases/download/0.3.0/clijvm-0.3.0.tar.gz"
+  sha256 "0cc26c628750293180c73685bf96f82b87b406d3e30210cdb94b5a96576b1cb3"
   license "Apache-2.0"
 
   # clijvm needs a full JDK (attach API + jcmd), which Homebrew's openjdk provides.
